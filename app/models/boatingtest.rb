76ce33e6d830cee09a21830aad011315f0ba172d
Mike Thurgood <mikecthurgood@gmail.com>
2019-08-31 18:21:50 +0100

    Done.

--- app/models/boatingtest.rb
+++ app/models/boatingtest.rb
@@ -1,3 +1,24 @@
 class BoatingTest
 
+    attr_accessor :student, :status, :instructor, :test_name
+
+    @@all = []
+
+    def initialize(test_name, student, status, instructor)
+        @test_name = test_name
+        @student = student
+        @status = status
+        @instructor = instructor
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+
+
+
+    
 end
--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,41 @@
 class Instructor
 
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def search_test(student, test_name)
+        BoatingTest.all.find {| test | test.student == student && test.test_name == test}
+    end
+
+    def pass_student(student, test_name)
+        exam = search_test(student, test_name)
+        if exam
+            test.status = "passed"
+        else
+            BoatingTest.new(test_name, student, status = "passed", self)
+        end
+    end
+
+    def fail_student(student, test_name)
+        exam = search_test(student, test_name)
+        if exam
+            test.status = "failed"
+        else
+            BoatingTest.new(test_name, student, status = "failed", self)
+        end
+    end
+
+
+
 end
