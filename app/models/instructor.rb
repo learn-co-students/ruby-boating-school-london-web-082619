d961e9e233e6a02fa1e8cc88299822f235b5abf9
Angus Morrison <contact@angus-morrison.com>
2019-08-31 13:45:26 +0100

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,50 @@
 class Instructor
+    attr_reader :name
+
+    @@all = []
+
+    ###### Instance methods ######
+    #Works!
+    def initialize(name)
+        @name = name
+
+        @@all << self
+    end
+
+    #Works!
+    def fetch_test(student, test_name)
+        BoatingTest.all.find do | test |
+            test.test_name == test_name && test.student == student
+        end
+    end
+
+    #Works!
+    def pass_student(student, test_name)
+        test = fetch_test(student, test_name)
+        if test
+            test.status = "passed"
+            return test
+        else
+            BoatingTest.new(student, self, test_name, "passed")
+        end
+    end
+
+    #Works!
+    def fail_student(student, test_name)
+        test = fetch_test(student, test_name)
+        if test
+            test.status = "failed"
+            return test
+        else
+            BoatingTest.new(student, self, test_name, "failed")
+        end
+    end
+
+
+    ###### Class methods ######
+    #Works!
+    def self.all()
+        @@all
+    end
 
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,38 @@
 class Student
+    attr_reader :first_name
+
+    @@all = []
+
+    ###### Instance methods ######
+    #Works!
+    def initialize(first_name)
+        @first_name = first_name
+
+        @@all << self
+    end
+
+    #Works!
+    def add_boating_test(instructor, test_name, status)
+        BoatingTest.new(self, instructor, test_name, status)
+    end
+
+    #Works!
+    def tests()
+        BoatingTest.all.select { | test | test.student == self }
+    end
+
+    #Works!
+    def grade_percentage
+        passed_tests = tests.select { | test | test.status == "passed" }
+        percent_pass = (passed_tests.length.to_f / tests().length) * 100
+        percent_pass.round(2)
+    end
+
+    ###### Class methods ######
+
+    #Works!
+    def self.find_student(first_name)
+        @@all.find { | student | student.first_name == first_name }
+    end
 
 end
