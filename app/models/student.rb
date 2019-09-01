e6b6da69871f13e09a76b012ddf14a2c6506ed3e
Michelle Van <mjtvan@gmail.com>
2019-09-01 23:59:19 +0100

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,34 @@
 class Student
+ @@all = [] 
+
+ attr_reader :first_name 
+
+ def initialize(first_name)
+    @first_name 
+    @@all << self 
+ end 
+
+ def self.all 
+    @@all 
+ end 
+
+def boating_test 
+    BoatingTest.all.select{|test| test.student == self}
+end 
+
+def add_boating_test(instructor, test_name, status)
+    BoatingTest.new(self, test_name, status, instructor) 
+end 
+
+def self.find_student(first_name_input)
+    @@all.find{|student| student.first_name == first_name_input}
+end 
+
+def grade_percentage
+    boating_test.count{|test| test.test_status == "passed"} / 
+    boating_test.count{|test| test.test_status}.to_f * 100 
+end 
+
+
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,9 +4,25 @@ def reload
   load 'config/environment.rb'
 end
 
-# Insert code here to run before hitting the binding.pry
-# This is a convenient place to define variables and/or set up new object instances,
-# so they will be available to test and play around with in your console
+student1 = Student.new("Spongebob")
+student2 = Student.new("Patrick")
+student3 = Student.new("Harry")
+student4 = Student.new("Jim")
+student5 = Student.new("Taylor")
+student6 = Student.new("Missy")
+ 
+instructor1 = Instructor.new("Ms.Puff")
+instructor2 = Instructor.new("Mr.Krabs")
+instructor3 = Instructor.new("Mr.Snow")
+instructor4 = Instructor.new("Mr.Hairy")
+ 
+boatingtest1 = student1.add_boating_test(instructor1, "Don't Crash 101", "pending")
+boatingtest2 = student2.add_boating_test(instructor1, "Power Steering 202", "failed")
+boatingtest2 = student2.add_boating_test(instructor2, "Power Steering 202", "failed")
+boatingtest2 = student2.add_boating_test(instructor3, "Power Steering 203", "failed")
+boatingtest3 = student1.add_boating_test(instructor2, "Power Steering 202", "passed")
+boatingtest4 = student2.add_boating_test(instructor4, "Power Steering 204", "passed")
+boatingtest5 = student3.add_boating_test(instructor2, "Power Steering 201", "passed")
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
